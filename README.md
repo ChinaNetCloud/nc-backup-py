88384e26c908189f5b5debd0b4f9b47ec5fde436
Abel Guzman Sanchez <abel.guzman@chinanetcloud.com>
2016-03-01 11:50:02 +0800

    created encryption class using OpenSSl AES library Crypto

---
+++ README.md
@@ -0,0 +1,11 @@
+ROAD MAP:
+-Pending Encryption
+-Storage
+-Communications (reports)
+-clean ups
+-DBS
+
+Next Release:
+-Scheduler
+-Improve Flow control
+-Other?
--- encryption/encryption.py
+++ encryption/encryption.py
@@ -1,4 +1,52 @@
-print "Encrypting files"
-"""
-cat "$KEY_FILE" "$FILE" | $GPGAGENT --daemon gpg2 --batch --yes --no-tty --quiet -c --passphrase-fd 0 | split  -d -b 4000m - "$FILE_GPG". #DL-NEW1#
-"""
+from hashlib import md5
+from Crypto.Cipher import AES
+from Crypto import Random
+
+
+class Encryption:
+    """OpenSSL encryption"""
+    def __derive_key_and_iv(self, password, salt, key_length, iv_length):
+        d = d_i = ''
+        while len(d) < key_length + iv_length:
+            d_i = md5(d_i + password + salt).digest()
+            d += d_i
+        return d[:key_length], d[key_length:key_length+iv_length]
+
+    def encrypt(self, in_file, out_file, password, key_length=32):
+        bs = AES.block_size
+        salt = Random.new().read(bs - len('Salted__'))
+        key, iv = self.__derive_key_and_iv(password, salt, key_length, bs)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        out_file.write('Salted__' + salt)
+        finished = False
+        while not finished:
+            chunk = in_file.read(1024 * bs)
+            if len(chunk) == 0 or len(chunk) % bs != 0:
+                padding_length = (bs - len(chunk) % bs) or bs
+                chunk += padding_length * chr(padding_length)
+                finished = True
+            out_file.write(cipher.encrypt(chunk))
+
+    def decrypt(self, in_file, out_file, password, key_length=32):
+        bs = AES.block_size
+        salt = in_file.read(bs)[len('Salted__'):]
+        key, iv = self.__derive_key_and_iv(password, salt, key_length, bs)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        next_chunk = ''
+        finished = False
+        while not finished:
+            chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
+            if len(next_chunk) == 0:
+                padding_length = ord(chunk[-1])
+                chunk = chunk[:-padding_length]
+                finished = True
+            out_file.write(chunk)
+
+# with open('/opt/backup/compressed/filesbackup_20160301_104659tar.gz', 'rb') as in_file, open('/opt/backup/encrypted/file.crypt', 'wb') as out_file:
+#     encrypt(in_file, out_file, '127869b49896ece244fc75c9d5b9b921')
+# with open('/opt/backup/encrypted/file.crypt', 'rb') as in_file, open('/opt/backup/ilesbackup_20160301_104659tar.gz', 'wb') as out_file:
+#     decrypt(in_file, out_file, '127869b49896ece244fc75c9d5b9b921')
+
+# print "Encrypting files"
+if __name__ == "__main__":
+    print "Encrypting files"
