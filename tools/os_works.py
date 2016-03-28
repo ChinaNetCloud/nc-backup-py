eba2bf7195993407ff547adb78d14ef10f4428f8
ryan lao <ryan.lao@chinanetcloud.com>
2016-03-28 09:37:46 +0000

    test

---
+++ tools/os_works.py
@@ -0,0 +1,10 @@
+import sys
+
+
+class OSInformation:
+    @staticmethod
+    def isWindows():
+        if getattr(sys, "getwindowsversion", None) is not None:
+            return sys.getwindowsversion()[0] == 6
+        else:
+            return False
