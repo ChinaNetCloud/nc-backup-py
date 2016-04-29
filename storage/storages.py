191315766ee877027ec34c3fd3083ac491c186a4
Abel Gusman Sanchez <cncuser@Abel-Guzman-Sanchezs-MacBook-Air.local>
2016-04-29 17:10:14 +0800

    fix bug un aws upload

--- storage/storages.py
+++ storage/storages.py
@@ -1,4 +1,3 @@
-import argparse
 import sys
 
 
@@ -31,11 +30,11 @@ class AWSS3(Storage):
 
     def upload_content(self, mypath_to_dir, bucket, client_host_name, upload_command='aws s3 cp'):
         print 'Uploading to storage S3'
-        files_to_upload = [mypath_to_dir + f for f in listdir(mypath_to_dir) if isfile(join(mypath_to_dir, f))]
+        files_to_upload = [f for f in listdir(mypath_to_dir) if isfile(join(mypath_to_dir, f))]
         sys.path.append(self.__home_path)
         from execution.subprocess_execution import SubprocessExecution
         for file_to_upload in files_to_upload:
-            aws_command = upload_command + ' '+ file_to_upload + ' s3://'+ bucket + '/' + client_host_name + '/'
+            aws_command = upload_command + ' '+ mypath_to_dir + '/' + file_to_upload + ' s3://'+ bucket + '/' + client_host_name + '/'
             execution_message = SubprocessExecution.main_execution_function(SubprocessExecution(), aws_command, True)
             SubprocessExecution.print_output(SubprocessExecution(), execution_message)
 
@@ -44,11 +43,3 @@ class AWSS3(Storage):
 
     def check_size_content(self):
         print 'checking the files size'
-
-
-# my_list = [Storage(), StorageLocal(), AWSS3()]
-#
-#
-#
-# for x in my_list:
-#     x.remove_content()
