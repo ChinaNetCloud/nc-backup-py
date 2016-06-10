53b32a023b107ddbb22a3c26baa59dcd626a3aa6
Abel Guzman <abel.guzman@chinanetcloud.com>
2016-06-10 12:44:59 +0800

    Improve postgres errors and loggin

--- postgres/postgres.py
+++ postgres/postgres.py
@@ -12,6 +12,7 @@ class PostgresBackup(ModuleFrame):
         self.__logger = logger
 
     def works_execution(self):
+        self.__execution_result = True
         if self.__parameters_dict['DESTINATION'] and self.__parameters_dict['PREFIX_FOLDER']:
             save_dir = self.__parameters_dict['DESTINATION'] \
                        + '/' + self.__parameters_dict['PREFIX_FOLDER'] + '/'
@@ -25,13 +26,28 @@ class PostgresBackup(ModuleFrame):
                 exclude_db.append('|')
             else:
                 exclude_db =['template0', 'template1', '|']
+
             # 'psql -l' produces a list of PostgreSQL databases.
-            get_list = os.popen('psql -l').readlines()
+            # get_list = os.popen('psql -l').readlines()
+            get_list = SubprocessExecution.main_execution_function(SubprocessExecution(),
+                                                                   'psql -l', True, self.__logger)
+            if get_list[0] == 0:
+                get_list = get_list[1].split('\n')
+            else:
+                self.__execution_result = False
+                self.__logger.critical('Error code: ' + str(get_list[0]))
+                self.__logger.critical('StdOut: ' + get_list[1])
+                self.__logger.critical('StdErr: ' + get_list[2])
+                # print get_list[2]
+                return 'Execution faile due to error listing postgres DBs'
             # Exclude header and footer lines.
-            db_list = get_list[3:-2]
+            db_list = get_list[3:-3]
+
+            # print db_list
             # Extract database names from first element of each row.
             for n in db_list:
                 n_row = string.split(n)
+                # print n_row
                 n_db = n_row[0]
                 # Pipe database dump through gzip
                 # into .gz files for all databases
@@ -40,5 +56,26 @@ class PostgresBackup(ModuleFrame):
                     pass
                 else:
                     print n_db
-                    os.popen('pg_dump ' + n_db + ' | gzip -c > ' + save_dir + n_db + '.gz')
+                    result_dump = \
+                        SubprocessExecution.main_execution_function(SubprocessExecution(),
+                                                                    'pg_dump ' + n_db + ' | gzip -c > ' + save_dir + n_db + '.gz'
+                                                                    , True, self.__logger)
+                    if result_dump[0] != 0:
+                        self.__execution_result = False
+                        self.__logger.critical('Error code: ' + str(result_dump[0]))
+                        self.__logger.critical('StdOut: ' + result_dump[1])
+                        self.__logger.critical('StdErr: ' + result_dump[2])
+                        # print result_dump[2]
+                        return 'Execution failed while executing: ' + 'pg_dump ' + n_db + ' | gzip -c > ' + save_dir + n_db + '.gz'
+                    # os.popen('pg_dump ' + n_db + ' | gzip -c > ' + save_dir + n_db + '.gz')
+
+        else:
+            self.__execution_result = False
+    # def __log_critical_error(self):
+
+    def output(self):
+        if self.__execution_result is True:
+            return 0, 'Success', ''
+        else:
+            return 1, '', 'Error'
 
