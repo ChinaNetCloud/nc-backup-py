a7e604699fc72f4fe52340440b5ae567f1b012ce
vagrant <vagrant@localhost.localdomain>
2016-08-05 10:54:53 +0800

    Setup.py development

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import pwd
 from shutil import move
 
 
@@ -6,12 +7,18 @@ from execution.config_parser import ConfigParser
 
 # Check that the script is running as root.
 if os.geteuid() != 0:
-    print "Error: This installer needs to be executed by root"
+    print "Error: The installer needs to be executed as superadmin"
     exit(1)
 
 # Create ncbackup if it does not exists.
 username = 'ncbackup'
-os.system("useradd -m %s -s /sbin/nologin" % username)
+
+try:
+    pwd.getpwnam(username)
+    print username + ' already exists.'
+except KeyError:
+    print 'User ' + username + ' does not exist. Adding it now'
+    os.system('useradd -m %s -s /sbin/nologin' % username)
 
 # Move default config files to /etc.
 configs_path = '/etc/nc-backup-py'
@@ -19,7 +26,6 @@ if not ConfigParser.check_exists(ConfigParser(),configs_path):
     move(os.getcwd() + '/conf/.', configs_path)
 else:
     move(os.getcwd() + '/conf/.', configs_path + '.')
-
 
 # make ncbackup the owner of /etc folder.
 
