eda6237b25465a2560fb49d53d6925db5a2878f1
root <root@localhost.localdomain>
2016-08-08 19:49:03 +0800

    Setup copy and ownership done.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
+import shutil
 import pwd
-from shutil import move
+import grp
 
 
 from execution.config_parser import ConfigParser
@@ -13,22 +14,59 @@ if os.geteuid() != 0:
 # Create ncbackup if it does not exists.
 username = 'ncbackup'
 
-try:
-    pwd.getpwnam(username)
-    print username + ' already exists.'
-except KeyError:
-    print 'User ' + username + ' does not exist. Adding it now'
-    os.system('useradd -m %s -s /sbin/nologin' % username)
+# try:
+#     pwd.getpwnam(username)
+#     print username + ' user already exists.'
+# except KeyError:
+#     print 'User ' + username + ' does not exist. Adding it now'
+os.system('useradd -m %s -s /sbin/nologin' % username)
+
+
+print 'Copy configs...'
+# Copy the files function
+def copy_files(source, destination, username):
+    # Os chown of the config folder
+    """
+    :rtype: object
+    """
+    uid = pwd.getpwnam(username).pw_uid
+    gid = grp.getgrnam(username).gr_gid
+    files = os.listdir(source)
+    files.sort()
+    os.chown(destination, uid, gid)
+    for f in files:
+        src = source + '/' + f
+        dst = destination + '/' + f
+        if os.path.isdir(src) == True:
+            try:
+                os.stat(dst)
+                os.chown(dst, uid, gid)
+            except:
+                os.mkdir(dst)
+                os.chown(dst, uid, gid)
+        else:
+            shutil.copy(src, dst)
+            os.chown(dst, uid, gid)
+
+# Current folder
+path = os.getcwd()
+path_with_conf = path + '/conf'
 
 # Move default config files to /etc.
 configs_path = '/etc/nc-backup-py'
-if not ConfigParser.check_exists(ConfigParser(),configs_path):
-    move(os.getcwd() + '/conf/.', configs_path)
-else:
-    move(os.getcwd() + '/conf/.', configs_path + '.')
+try:
+    os.stat(configs_path)
+except:
+    os.mkdir(configs_path)
+# Copy the configs to /etc
+copy_files(path_with_conf, configs_path, username)
 
-# make ncbackup the owner of /etc folder.
-
-
-
+# Copy code to var/lib
+destination_code_path = '/var/lib/nc-backup-py'
+try:
+    os.stat(destination_code_path)
+except:
+    os.mkdir(destination_code_path)
+# Copy files...
+copy_files(path, destination_code_path, username)
 
