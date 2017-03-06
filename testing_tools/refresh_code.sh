5f99fbe56bedd09539d275f608ec3b366dca4222
sudersan.raghul <sudersan.raghul@chinanetcloud.com>
2017-03-06 17:42:28 +0800

    Edit script to refresh testbed code.

--- testing_tools/refresh_code.sh
+++ testing_tools/refresh_code.sh
@@ -3,22 +3,22 @@
 GIT="/usr/bin/git"
 TAR="/bin/tar"
 PYTHON=`which python`
-CONFIG_DIR=""
+CONFIG_DIR="$HOME/nc-backup-py-configs"
 
 echo "Using python: $PYTHON"
 
 echo "Back up config dir $CONFIG_DIR"
 
 if [ ! -d "$CONFIG_DIR" ];then
-  echo "$HOME/nc-backup-py-configs/ does not exist creating..."
+  echo "$CONFIG_DIR does not exist creating..."
   mkdir $CONFIG_DIR
 fi
 
-tar -czvf $HOME/nc-backup-py-configs/nc-backup-py.`date +%Y%m%d_%H%M%S`.bkp.tar /etc/nc-backup-py
+tar -czvf $CONFIG_DIR/nc-backup-py.`date +%Y%m%d_%H%M%S`.bkp.tar /etc/nc-backup-py
 
 echo "Git: Pulling latest commit from $(git config --get remote.origin.url)"
 
-GIT pull
+$GIT pull
 
 
 if [ -z $BRANCH ];then
@@ -28,4 +28,4 @@ fi
 
 echo "Installing latest update on branch $BRANCH"
 
-PYTHON setup.py
+$PYTHON setup.py
