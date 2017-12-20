ed91857a577f5ea0f34d3152b91db441e4620908
raghulmz <raghulmz@gmail.com>
2017-12-20 16:14:59 +0800

    Streamline storage wip

--- nc-backup-py/storage/storage.py
+++ nc-backup-py/storage/storage.py
@@ -1,37 +1,97 @@
-import argparse
-import sys
-
+"""Storage Execution Classes."""
 
 
 class StorageExecution:
 
-    def storage_commands(self):
+    def __init__(self):
+        """Initalize and run."""
+        import sys
+        import os
+        print "os.getcwd(): %s" % os.getcwd()
+        self.storage_cmd = self.get_args()
+        self.set_defaults()
+        sys.path.append(self.storage_cmd.HOME_FOLDER)
+
+    def get_args(self):
+        """Get arguments to script."""
+        import argparse
         parser_object = argparse.ArgumentParser()
-        parser_object.add_argument('-o', '--OBJECTIVES', '--TARGETS', type=str
-                                   , help='Objectives to encrypt', required=False)
-        parser_object.add_argument('-l', '--LOCAL_BACKUP', type=str
-                                   , help='Objectives to encrypt', required=True)
-        parser_object.add_argument('-H', '--HOME_FOLDER', type=str
-                                   , help='Path to the whole project folder, '
-                                          'to include other libruaries', required=True)
-        parser_object.add_argument('-D', '--DESTINATION', type=str
-                                   , help='Backup destination: local, s3, oss, etc', required=True)
-        parser_object.add_argument('-b', '--BUCKET_NAME', type=str
-                                   , help='Backup destination e.g: nc-backup-kr', required=False)
-        parser_object.add_argument('-hn', '--HOSTNAME', type=str
-                                   , help='Server name (client Host Name) e.g: nc-backup-kr', required=True)
-        parser_object.add_argument('-u', '--UPLOAD_COMMAND', type=str
-                                   , help='AWS upload command', required=False)
-        parser_object.add_argument('-R', '--REMOVE_OBJECTIVES', '--REMOVE_TARGETS', type=str
-                                   , help='Remove Encrypted files and folder after execution', required=False)
-        parser_object.add_argument('-A', '--ALIYUN_CREDENTIALS', type=str
-                                   , help='Remove Encrypted files and folder', required=False)
-        parser_object.add_argument('--CUSTOM_COMMAND_TEMPLATE', type=str
-                                   , help='Custom Command Template', required=False)
-        parser_object.add_argument('--CUSTOM_COMMAND_DICT', type=str
-                                   , help='Custom Command Dictionary', required=False)
+        parser_object.add_argument(
+            '-o', '--OBJECTIVES', '--TARGETS',
+            type=str,
+            help='Objectives to encrypt',
+            required=False
+            )
+        parser_object.add_argument(
+            '-D', '--DESTINATION',
+            type=str,
+            help='Backup destination: local, s3, oss, etc',
+            required=True)
+        parser_object.add_argument(
+            '-H', '--HOME_FOLDER',
+            type=str,
+            help='Path to the nc-backup-py folder',
+            required=True)
+        parser_object.add_argument(
+            '-hn', '--HOSTNAME',
+            type=str,
+            help='Server name (client Host Name) e.g: nc-backup-kr',
+            required=True)
+        parser_object.add_argument(
+            '-u', '--UPLOAD_COMMAND_TEMPLATE',
+            type=str,
+            help='Upload command template.',
+            required=False)
+        parser_object.add_argument(
+            '-l', '--LS_COMMAND_TEMPLATE',
+            type=str,
+            help='List Command Template',
+            required=False)
+        parser_object.add_argument(
+            '-r', '--RM_COMMAND_TEMPLATE',
+            type=str,
+            help='List Command Template',
+            required=False)
+        parser_object.add_argument(
+            '-R', '--REMOVE_OBJECTIVES',
+            '--REMOVE_TARGETS',
+            type=str,
+            help='Remove Encrypted files and folder after execution',
+            required=False)
+        parser_object.add_argument(
+            '--ARGS_DICT',
+            type=str,
+            help='ARGS Dictionary',
+            required=False)
+        parser_object.add_argument(
+            '--DEFAULTS_FILE',
+            type=str,
+            help='Default Templates File',
+            required=False,
+            default="storage_templates.json")
         args_list, unknown = parser_object.parse_known_args()
         return args_list
+
+    def set_defaults(self):
+        """Set defaults if not present."""
+        if not self.storage_cmd.REMOVE_OBJECTIVES:
+            self.storage_cmd.REMOVE_OBJECTIVES = 'True'
+            print "WARNING: REMOVE_OBJECTIVES is missing, default is true."
+        if not self.storage_cmd.OBJECTIVES:
+            self.storage_cmd.OBJECTIVES = '/opt/backup/encrypted'
+        print 'Storage Destination: ' + self.storage_cmd.DESTINATION
+
+        if self.storage_cmd.UPLOAD_COMMAND_TEMPLATE:
+            print "Using template %s " % self.storage_cmd.UPLOAD_COMMAND_TEMPLATE
+        else:
+            print "Trying to set default %s storage upload functions" % self.storage_cmd.DESTINATION
+            if str(self.storage_cmd.DESTINATION) not in ['s3', 'oss', 'ssh', 'local']:
+                print ('Upload command template for not available storage destination: %s ' % self.storage_cmd.DESTINATION)
+                print 'You can use your own template for the upload command. See:'
+                print '<add link to docs>'
+                exit(1)
+            else:
+                print "Sucessfully set Upload command template for %s." % self.storage_cmd.DESTINATION
 
     def iterate_resut(self, uploads_to_cloud):
         succesful = 0
@@ -44,60 +104,18 @@ class StorageExecution:
                 exit(1)
             count_file = count_file + 1
 
-if __name__ == "__main__":
-    storage_cmd = StorageExecution.storage_commands(StorageExecution())
-    sys.path.append(storage_cmd.HOME_FOLDER)
-    from tools.filesystem_handling import FilesystemHandling, remove_objectives
-    from execution.subprocess_execution import SubprocessExecution
-    from execution.config_parser import ConfigParser
-    if str(storage_cmd.DESTINATION) not in ['s3', 'oss', 'ssh', 'local', 'azure', 'custom_command']:
-        print 'Not supported storage destination: ' + str(storage_cmd.DESTINATION)
-        exit(1)
-
-    if not ConfigParser.check_exists(ConfigParser(), storage_cmd.REMOVE_OBJECTIVES):
-        storage_cmd.REMOVE_OBJECTIVES = 'True'
-    if not ConfigParser.check_exists(ConfigParser(), storage_cmd.OBJECTIVES):
-        storage_cmd.OBJECTIVES = '/opt/backup/encrypted'
-    print 'Executing backup files type: ' + storage_cmd.DESTINATION
-    if storage_cmd.DESTINATION == 'local':
-        command_move = 'cp ' + storage_cmd.OBJECTIVES + '/* ' + storage_cmd.LOCAL_BACKUP
-        FilesystemHandling.create_directory(storage_cmd.LOCAL_BACKUP)
-        ExecuteBackup = SubprocessExecution.main_execution_function(SubprocessExecution(), command_move)
-        remove_objectives(storage_cmd.OBJECTIVES, storage_cmd.REMOVE_OBJECTIVES)
-    elif storage_cmd.DESTINATION == 's3':
-        print "calling S3 storage upload functions"
-        from storages import AWSS3
-        uploads_to_s3 = AWSS3.upload_content(AWSS3(storage_cmd.HOME_FOLDER),storage_cmd.OBJECTIVES,
-                                            storage_cmd.BUCKET_NAME, storage_cmd.HOSTNAME,
-                                            storage_cmd.UPLOAD_COMMAND, storage_cmd.REMOVE_OBJECTIVES)
-        succesful = 0
-        count_file = 1
-        if uploads_to_s3:
-            StorageExecution.iterate_resut(StorageExecution(), uploads_to_s3)
-        else:
-            print 'Executing OSS upload retuned a None result'
-
-    elif storage_cmd.DESTINATION == 'oss':
-        print "calling OSS storage upload functions"
-        from storages import AliyunOSS
-        print storage_cmd.BUCKET_NAME
-        uploads_to_oss = AliyunOSS.upload_content(AliyunOSS(storage_cmd.HOME_FOLDER), storage_cmd.ALIYUN_CREDENTIALS,
-                                                  storage_cmd.OBJECTIVES, storage_cmd.BUCKET_NAME,
-                                                  storage_cmd.HOSTNAME, storage_cmd.REMOVE_OBJECTIVES)
-        if uploads_to_oss:
-            StorageExecution.iterate_resut(StorageExecution(), uploads_to_oss)
-        else:
-            print 'Executing OSS upload retuned a None result'
-
-    elif storage_cmd.DESTINATION == 'ssh':
-        print "calling SSH storage upload functions"
-
-    elif storage_cmd.DESTINATION == 'custom_command':
-        print "calling custom command to upload."
-        from storages import CustomCommand
-        custom_command_upload = CustomCommand(storage_cmd)
+    def execute_upload(self):
+        print "calling %s command to upload." % self.storage_cmd.DESTINATION
+        from storages import Storage
+        custom_command_upload = Storage(self.storage_cmd)
         custom_uploads = custom_command_upload.execute()
         if custom_uploads:
             StorageExecution.iterate_resut(StorageExecution(), custom_uploads)
         else:
-            print 'Executing Custom upload retuned a None result'
+            print 'Executing %s upload retuned a None result' % self.storage_cmd.DESTINATION
+
+
+if __name__ == "__main__":
+
+    executor = StorageExecution()
+    executor.execute_upload()
--- nc-backup-py/storage/storages.py
+++ nc-backup-py/storage/storages.py
@@ -7,6 +7,17 @@ from os.path import isfile, join
 from tools.filesystem_handling import remove_objectives
 
 class Storage:
+    """"""
+
+    def __init__(self, storage_cmd):
+        """Get and store arguments."""
+        self.__args = storage_cmd
+        self.__checkArgs()
+
+    def __checkArgs(self):
+        self.__custom_command_dict = eval(self.__args.ARGS_DICT)
+        self.__custom_command_dict["OBJECTIVES"] = self.__args.OBJECTIVES
+        self.__custom_command_dict["HOSTNAME"] = self.__args.HOSTNAME
 
     def list_content(self):
         print "Listing directory content"
@@ -20,223 +31,6 @@ class Storage:
     def check_size_content(self):
         print 'checking the files size'
 
-
-class AWSS3(Storage):
-    def __init__(self, home_path=''):
-        self.__home_path = home_path
-        # self.__subprocess_upload = SubprocessExecution()
-
-    def list_content(self):
-        print "Listing directory content"
-
-    @staticmethod
-    def test_aws_connectivity(home_path, bucket_name):
-        aws_s3_ls_command = 'aws s3 ls %s' % bucket_name
-        sys.path.append(home_path)
-        from execution.subprocess_execution import SubprocessExecution
-        result_aws_s3_ls = SubprocessExecution.main_execution_function(SubprocessExecution(), aws_s3_ls_command)
-        # print result_aws_s3_ls
-        # print type(result_aws_s3_ls[1])
-        # print bucket_name
-        # print result_aws_s3_ls[1].find(bucket_name)
-        # print type(result_aws_s3_ls[1].find(bucket_name))
-        # print result_aws_s3_ls[0]
-        if result_aws_s3_ls[0] == 255 and result_aws_s3_ls[1].find("Unable to locate credentials."):
-            return 'No credentials'
-        elif result_aws_s3_ls[0] == 255:
-            return 'Other error. Check if Bucket exists.'
-        return 'Ok'
-
-    def upload_content(self, mypath_to_dir, bucket, client_host_name, upload_command='aws s3 cp',
-                       remove_objective='False', aws_s3_test=True):
-        if upload_command is None:
-            upload_command = 'aws s3 cp'
-        aws_and_bucket_test = self.test_aws_connectivity(self.__home_path, bucket)
-        print aws_and_bucket_test
-        if aws_s3_test == True and aws_and_bucket_test == 'No credentials':
-            print 'The can not execute AWS CLI test; please check installation and credentials. ' \
-                  'Stopping execution of AWS upload'
-            exit(1)
-        elif aws_s3_test == True and aws_and_bucket_test == 'Bucket not Found':
-            print 'Can not find the AWS bucket name:'+ bucket +' Please check configs and try again'
-            exit(1)
-        print 'Uploading to storage S3'
-        files_to_upload = [f for f in listdir(mypath_to_dir) if isfile(join(mypath_to_dir, f))]
-        sys.path.append(self.__home_path)
-        from execution.subprocess_execution import SubprocessExecution
-        execution_message = []
-        for file_to_upload in files_to_upload:
-            aws_command = upload_command + ' '+ mypath_to_dir + '/' + file_to_upload + ' s3://'+ bucket + '/' \
-                          + client_host_name + '/' + file_to_upload
-            count = 1
-            time_retry = 60
-            while count <= 5:
-                print 'Trying upload attempt number: ' + str(count)
-                # try:
-                tmp_execution_message = SubprocessExecution.main_execution_function(SubprocessExecution(), aws_command)
-                # except
-                # print tmp_execution_message
-                time_retry = time_retry * count
-                if tmp_execution_message[0] == 0:
-                    print 'Upload attempt ' + str(count) + ' successful.'
-                    break
-                elif tmp_execution_message[0] == 127:
-                    print "Error with AWS CLI binary. Is it installed?"
-                    break
-                # NEED TO DETERMINE: is
-                # elif tmp_execution_message[0] == 1:
-                #     print "Error with AWS CLI binary. Is it installed?"
-                #     break
-                else:
-                    print 'Upload attempt number: ' + str(count) + ' FAILED for: ' + aws_command
-                    print 'StdOut: ' + str(tmp_execution_message[0])
-                    print 'StdErr: ' + str(tmp_execution_message[0])
-                    print 'We will wait for: ' + str(time_retry/60) + ' minute(s) before upload attempt number: ' + \
-                          str(count + 1)
-                    time.sleep(time_retry)
-                count = count + 1
-            execution_message.append(tmp_execution_message)
-
-        remove_objectives(mypath_to_dir, remove_objective)
-        return execution_message
-
-    def remove_content(self):
-        print 'S3: removing files from storage'
-
-    def check_size_content(self):
-        print 'checking the files size'
-
-class AliyunOSS(Storage):
-    def __init__(self, home_path=''):
-        self.__home_path = home_path
-
-        # self.__subprocess_upload = SubprocessExecution()
-
-    def list_content(self):
-        print "Listing directory content"
-
-    def upload_content(self, credentials_file, mypath_to_dir, bucket='', client_host_name='', remove_objective='False'):
-        print 'Trying uploading to Aliyun OSS bucket:'
-        if bucket is not None and bucket != '':
-            print bucket
-        files_to_upload = [f for f in listdir(mypath_to_dir) if isfile(join(mypath_to_dir, f))]
-        # import oss2
-        # credential_dict = self.__read_credentials_from_file(credentials_file)
-        # if bucket is not None and bucket != '':
-        #     credential_dict['bucket'] = bucket
-        # auth = oss2.Auth(credential_dict['access_id'], credential_dict['access_key'])
-        #
-        # bucket = oss2.Bucket(auth, credential_dict['host'], credential_dict['bucket'])
-        execution_message = []
-        sys.path.append(self.__home_path)
-        from execution.subprocess_execution import SubprocessExecution
-        for file_to_upload in files_to_upload:
-            tmp_result_execution = ''
-            local_file = mypath_to_dir + '/' + file_to_upload
-            # print local_file
-            count = 1
-            time_retry = 60
-            while count <= 5:
-                # try:
-                # print client_host_name + '/' + file_to_upload
-                alicmd_string = 'alicmd -u ' + local_file
-                # tmp_result_execution = bucket.put_object_from_file (client_host_name + '/' + file_to_upload, local_file)
-                # except:
-                #     print 'Attempt failed'
-                # print('status={0}, request_id={1}'.format(e.status, e.request_id))
-                tmp_execution_message = SubprocessExecution.main_execution_function(SubprocessExecution(), alicmd_string)
-
-                time_retry = time_retry * count
-                if tmp_execution_message[0] == 0:
-                    print 'Upload attempt ' + str(count) + ' successful.'
-                    break
-                elif tmp_execution_message[0] == 127:
-                    print "Error with AWS CLI binary. Is it installed?"
-                    break
-                # NEED TO DETERMINE: is
-                # elif tmp_execution_message[0] == 1:
-                #     print "Error with AWS CLI binary. Is it installed?"
-                #     break
-                else:
-                    print 'Upload attempt number: ' + str(count) + ' FAILED for: ' + aws_command
-                    print 'StdOut: ' + str(tmp_execution_message[0])
-                    print 'StdErr: ' + str(tmp_execution_message[0])
-                    print 'We will wait for: ' + str(time_retry/60) + ' minute(s) before upload attempt number: ' + \
-                          str(count + 1)
-                    time.sleep(time_retry)
-                # if tmp_result_execution and tmp_result_execution.status == 200:
-                #     message_return = 'Status: ' + str(tmp_result_execution.status) + ' Request ID: ' + \
-                #                      str(tmp_result_execution.request_id) + tmp_result_execution.etag
-                #     status_success = 0
-                #     print 'Upload attempt ' + str(count) + ' successful.'
-                #     break
-                # else:
-                #     status_success = 1
-                #     print 'Upload attempt number: ' + str(count) + ' FAILED for: ' + local_file
-                #     print tmp_result_execution
-                #     # print tmp_result_execution.headers
-                #     print 'We will wait for: ' + str(time_retry / 60) + ' minute(s) before upload attempt number: ' + \
-                #           str(count + 1)
-                #     time.sleep(time_retry)
-                count = count + 1
-            # execution_message.append((status_success, message_return, ''))
-            execution_message.append(tmp_execution_message)
-
-        sys.path.append(self.__home_path)
-        remove_objectives(mypath_to_dir, remove_objective)
-        return execution_message
-
-    def remove_content(self):
-        print 'S3: removing files from storage'
-
-
-    def check_size_content(self):
-        print 'checking the files size'
-
-    def __read_credentials_from_file(self, file_to_read):
-        print 'Now we read the Aliyun credentials from the file: ' + file_to_read
-        myvars = {}
-        with open(file_to_read, 'r') as file_loaded:
-            print file_loaded.name
-            for line in file_loaded:
-                if line  and line is not '' and '[' not in line and ']' not in line and not line.startswith('#'):
-                    name, var = line.partition("=")[::2]
-                    myvars[name.strip()] = str(var).strip().rstrip()
-        # print myvars
-        return myvars
-
-
-class SSHStorage(Storage):
-    def __init__(self, home_path=''):
-        self.__home_path = home_path
-
-    def upload_content(self, key_):
-        pass
-        """import scp
-
-            client = scp.Client(host=host, user=user, keyfile=keyfile)
-            # or
-            client = scp.Client(host=host, user=user)
-            client.use_system_keys()
-            # or
-            client = scp.Client(host=host, user=user, password=password)
-
-            # and then
-            client.transfer('/etc/local/filename', '/etc/remote/filename')
-        """
-
-class CustomCommand(Storage):
-
-    def __init__(self, storage_cmd):
-        """Get and store arguments."""
-        self.__args = storage_cmd
-        self.__checkArgs()
-
-    def __checkArgs(self):
-        self.__custom_command_dict = eval(self.__args.CUSTOM_COMMAND_DICT)
-        self.__custom_command_dict["OBJECTIVES"] = self.__args.OBJECTIVES
-        self.__custom_command_dict["HOSTNAME"] = self.__args.HOSTNAME
-
     def execute(self):
         from execution.subprocess_execution import SubprocessExecution
         files_to_upload = [f for f in listdir(self.__args.OBJECTIVES)
@@ -249,7 +43,7 @@ class CustomCommand(Storage):
 
             while count <= 5:
                 print 'Trying upload attempt number: ' + str(count)
-                command = self.__args.CUSTOM_COMMAND_TEMPLATE % self.__custom_command_dict
+                command = self.__args.UPLOAD_COMMAND_TEMPLATE % self.__custom_command_dict
                 print "Executing external command: %s " % command
                 tmp_execution_message = SubprocessExecution.main_execution_function(
                     SubprocessExecution(),
