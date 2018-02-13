1418c4a506ffc5eb9707b7d2905957813bb17665
raghulmz <raghulmz@gmail.com>
2018-02-13 18:23:27 +0800

    add test install script.

--- Jenkins/test_install.sh
+++ Jenkins/test_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo ed 's|^mirrorlist|#mirrorlist|g' /etc/yum.repos.d/CentOS-* -i
-sudo ed 's|^#baseurl|baseurl|g' /etc/yum.repos.d/CentOS-* -i
-sudo ed 's|mirror.centos.org|mirrors.aliyun.com|g' /etc/yum.repos.d/CentOS-* -i
-sudo ed 's|enabled=1|enabled=0|g' /etc/yum/pluginconf.d/fastestmirror.conf -i
-sudo um install https://mirrors.aliyun.com/epel/epel-release-latest-7.noarch.rpm -y
-sudo um updateinfo && yum install git python python-pip python-wheel python-crypto sudo -y
-sudo um groupinstall 'Development Tools' -y
+sudo sed 's|^mirrorlist|#mirrorlist|g' /etc/yum.repos.d/CentOS-* -i
+sudo sed 's|^#baseurl|baseurl|g' /etc/yum.repos.d/CentOS-* -i
+sudo sed 's|mirror.centos.org|mirrors.aliyun.com|g' /etc/yum.repos.d/CentOS-* -i
+sudo sed 's|enabled=1|enabled=0|g' /etc/yum/pluginconf.d/fastestmirror.conf -i
+sudo yum install https://mirrors.aliyun.com/epel/epel-release-latest-7.noarch.rpm -y
+sudo yum updateinfo && yum install git python python-pip python-wheel python-crypto sudo -y
+sudo yum groupinstall 'Development Tools' -y
 # git clone https://github.com/ChinaNetCloud/nc-backup-py.git
 # cd nc-backup-py
-sudo it checkout jenkins-setup
-sudo ip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade .
+sudo git checkout jenkins-setup
+sudo pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade .
