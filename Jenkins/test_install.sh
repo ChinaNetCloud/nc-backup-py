9c622d7f7d8e7581c8a7d18710d5a9e38c3c5c51
raghulmz <raghulmz@gmail.com>
2018-02-13 18:02:00 +0800

    add test install script.

--- Jenkins/test_install.sh
+++ Jenkins/test_install.sh
@@ -7,8 +7,8 @@ sed 's|enabled=1|enabled=0|g' /etc/yum/pluginconf.d/fastestmirror.conf -i
 yum install https://mirrors.aliyun.com/epel/epel-release-latest-7.noarch.rpm -y
 yum updateinfo && yum install git python python-pip python-wheel python-crypto sudo -y
 yum groupinstall 'Development Tools' -y
-git clone https://github.com/ChinaNetCloud/nc-backup-py.git
-cd nc-backup-py
+# git clone https://github.com/ChinaNetCloud/nc-backup-py.git
+# cd nc-backup-py
 git checkout jenkins-setup
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade .
 su - nc-backup-py
