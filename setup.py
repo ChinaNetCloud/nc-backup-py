30d797e7decffac61c709008118f68c534d927a2
raghulmz <raghulmz@gmail.com>
2018-02-28 10:47:36 +0800

    minor fix

--- setup.py
+++ setup.py
@@ -174,6 +174,8 @@ class Setup_nc_backup_py(install):
         # Copy s3 backup as default config
         sample = os.path.join(CONFIG_PATH, "Examples/conf.s3.json")
         default = os.path.join(CONFIG_PATH, "conf.json")
+        print sample
+        print default
         logging.info("Copy default config from %s to %s") % (sample, default)
         copy_files(sample, default, uid=uid, gid=gid)
         sed(os.path.join(CONFIG_PATH, "conf.json"), r"srv-nc-template-host-config", hostname)
